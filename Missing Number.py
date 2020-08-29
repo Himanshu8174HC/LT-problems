689b86742520d9a428ea39fba5d62729e6b9a883
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-08-29 18:03:20 +0530

    Update Missing Number.py

--- Missing Number.py
+++ Missing Number.py
@@ -2,3 +2,15 @@ class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
         return ((n*(n+1))//2 - sum(nums))
+    
+    
+    
+    
+    #######################
+    
+    se1 = set(list(range(len(nums))))
+        res = list(se1-set(nums))
+        if len(res)==1:
+            return res[0]
+        else:
+            return len(nums)
