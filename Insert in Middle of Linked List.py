7cfb8e8ac62a6dc74ebcec1d13802c7b137dffb7
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-09-18 17:35:04 +0530

    Create Insert in Middle of Linked List.py

---
+++ Insert in Middle of Linked List.py
@@ -0,0 +1,24 @@
+def insertInMid(head,node):
+    #code here
+    
+    curr = head
+    prev = None
+    if (head == None):
+        head = node
+        return head
+    
+    
+    if n%2 == 0:
+        count = n//2
+    else:
+        count = n//2+1
+    n1 = 0
+    curr = head
+    while n1 != count :
+        prev = curr
+        curr = curr.next
+        n1 = n1+1
+    
+    prev.next = node
+    node.next = curr
+    return head
