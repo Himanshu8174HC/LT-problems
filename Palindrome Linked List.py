74fbaf9dab8c7fe7f97931437b54f1ceaf33e13c
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-09-21 12:04:55 +0530

    Update Palindrome Linked List.py

--- Palindrome Linked List.py
+++ Palindrome Linked List.py
@@ -9,3 +9,19 @@ class Solution:
         if l == l[::-1]:
             return True
         return False
+    
+################################################BEST 
+class Solution:
+    def isPalindrome(self, head: ListNode) -> bool:
+        number = 0
+        reverse = 0
+        
+        curr = head
+        multiplier = 1
+        while(curr):
+            number = number * 10 + curr.val # append curr.val to the end of number
+            reverse = curr.val * multiplier + reverse # append curr.val to the beginning of reverse
+            
+            curr = curr.next
+            multiplier *= 10
+            
