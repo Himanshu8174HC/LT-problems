5919b140adc872a6eb255b0926402f17bfe02841
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-09-24 11:50:39 +0530

    Create Merge Two Sorted Lists.py

---
+++ Merge Two Sorted Lists.py
@@ -0,0 +1,32 @@
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        S=None
+        P=l1
+        Q=l2
+        
+        if l1 and l2:
+            if l1.val<=l2.val:
+                S=P
+                P=S.next
+            else:
+                S=Q
+                Q=S.next
+            new_Node = S
+        while P and Q:
+            if P.val<=Q.val:
+                S.next = P
+                S = P
+                P = S.next
+            else:
+                S.next = Q
+                S =Q
+                Q =S.next
+        if not P:
+            S.next = Q
+        if not Q:
+            S.next = P
+        return new_Node
