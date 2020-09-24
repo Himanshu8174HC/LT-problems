1d6fc6f54ca8d5ec57ef94f0cdf54b8cd6fd6fe4
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-09-24 16:18:39 +0530

    Update Swap Nodes in Pairs.py

--- Swap Nodes in Pairs.py
+++ Swap Nodes in Pairs.py
@@ -16,4 +16,19 @@ class Solution:
                 P = temp
         return new
     
+ ########################################More Efficient
+class Solution:
+    def swapPairs(self, head: ListNode) -> ListNode:
+        dummyhead=prev=ListNode(0)
+        prev.next=head
+        while prev.next and prev.next.next:
+            a=prev.next
+            b=prev.next.next
+            c=prev.next.next.next
+            prev.next=b
+            prev.next.next=a
+            prev.next.next.next=c
+            prev=prev.next.next
+        return dummyhead.next
+    
     
