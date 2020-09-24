bd7d8665b31e9a5e567e44d6d7b77c3375fd5d50
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-09-24 12:43:49 +0530

    Update Swap Nodes in Pairs.py

--- Swap Nodes in Pairs.py
+++ Swap Nodes in Pairs.py
@@ -1,16 +1,19 @@
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
+        if head is None or head.next is None:
+            return head
         P = head
-        new = head.next
-        while head:
+        new = P.next
+        while 1:
             Q = P.next
             temp = Q.next
             Q.next = P
             if temp == None or temp.next == None:
                 P.next = temp
                 break
-            
-            P.next = temp.next
-            P = temp
+            else:
+                P.next = temp.next
+                P = temp
         return new
+    
     
