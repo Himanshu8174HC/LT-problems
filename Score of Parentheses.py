386e7a1156f6de6b8660b1a97310cec7e466a315
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-10-28 16:01:50 +0530

    Update Score of Parentheses.py

--- Score of Parentheses.py
+++ Score of Parentheses.py
@@ -13,3 +13,18 @@ class Solution:
                     flag = 0                
                 stack.pop()
         return count 
+    
+    
+    ################################################################More efficient
+    class Solution:
+    def scoreOfParentheses(self, S: str) -> int:
+        stack = [0] 
+
+        for x in S:
+            if x == '(':
+                stack.append(0)
+            else:
+                v = stack.pop()
+                stack[-1] += max(2 * v, 1)
+
+        return stack.pop()
