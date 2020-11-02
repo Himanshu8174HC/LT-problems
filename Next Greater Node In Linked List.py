19fda5d764b9e859057e91e8e921615011baff76
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-11-02 21:50:00 +0530

    Create Next Greater Node In Linked List.py

---
+++ Next Greater Node In Linked List.py
@@ -0,0 +1,15 @@
+class Solution:
+    def nextLargerNodes(self, head: ListNode) -> List[int]:
+        pos = -1
+        stack =[]
+        ans =[]
+        while head:
+            pos += 1
+            ans.append(0)
+            while stack and stack[-1][1] < head.val:
+                idx, _ = stack.pop()
+                ans[idx] = head.val
+                
+            stack.append((pos, head.val))
+            head = head.next
+        return ans
