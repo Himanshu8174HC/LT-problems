59168a795291354f2f7acf54d128db67f1c1c881
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-12-01 11:24:22 +0530

    Update Find the Most Competitive Subsequence.py

--- Find the Most Competitive Subsequence.py
+++ Find the Most Competitive Subsequence.py
@@ -2,7 +2,8 @@ class Solution:
     def mostCompetitive(self, nums: List[int], k: int) -> List[int]:
         stack = []
         for i in range(len(nums)):
-            while stack and stack[-1] > nums[i] and len(stack) + len(nums)-i > k:
-                stack.pop()
-            stack.append(nums[i])
-        return stack[:k]
+            while stack and stack[-1] > nums[i] and len(stack) + len(nums)-i > k: # {len(stack) + len(nums)-i > k} => if we add element in stack and enough elemnt in the remaining element in the array is greator than k then we we keep poping
+                stack.pop() #pop stack only if array have enugh element
+            if len(stack)< k:
+                stack.append(nums[i])
+        return stack
