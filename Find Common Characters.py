f27ea8bb144e5b3b50caa319429a6690ecc558c6
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2020-12-20 10:59:09 +0530

    Create Find Common Characters.py

---
+++ Find Common Characters.py
@@ -0,0 +1,15 @@
+class Solution:
+    def commonChars(self, A: List[str]) -> List[str]:
+        d1 = Counter(A[0])
+        for i in A:
+            d2 = Counter(i)
+            for k in d1:
+                if k in d2:
+                    d1[k] = min(d1[k], d2[k])
+                else:
+                    d1[k] = 0
+        ans = []
+        for k, v in d1.items():
+            if v != 0:
+                ans += [k]*v
+        return ans
