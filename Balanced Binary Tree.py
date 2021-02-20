3022f92cbb6912385bc8fb7ec97732cf48cf98f1
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2021-02-20 21:35:37 +0530

    Create  Balanced Binary Tree.py

---
+++ Balanced Binary Tree.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isBalanced(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+
+        return abs(self.getHeight(root.left) - self.getHeight(root.right)) < 2 and self.isBalanced(root.left) and self.isBalanced(root.right)
+
+    def getHeight(self, root):
+        if not root:
+            return 0
+
+        return 1 + max(self.getHeight(root.left), self.getHeight(root.right))
+        
+            
