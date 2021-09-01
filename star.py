ad1880c6687f0ec6ec086289197439255bb5cfec
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2021-09-01 16:58:33 +0530

    Delete star.py

--- star.py
+++
@@ -1,8 +0,0 @@
-n=int(input("enter the number: "))
-for row in range (0,n):
-    for col in range (0,n):
-        if row==0 or row-col==0 or col == n-1:
-            print("*",end="")
-        else:
-            print(end=" ")
-    print()
