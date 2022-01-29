5218606aa0bb181a69ab4d1855f9b617579d1c07
Himanshu <67467171+Himanshu8174HC@users.noreply.github.com>
2022-01-30 00:22:33 +0530

    Update Count Elements With Strictly Smaller and Greater Elements.py

--- Count Elements With Strictly Smaller and Greater Elements.py
+++ Count Elements With Strictly Smaller and Greater Elements.py
@@ -9,3 +9,16 @@ class Solution:
                 ans -= 1
                 
         return ans
+   
+
+############################################################O(N)
+ans = 0
+        Max_nums = max(nums)
+        Min_nums = min(nums)
+        
+        for i in range(len(nums)):
+            if Min_nums < nums[i] < Max_nums:
+                ans += 1
+        return ans
+         
+        
